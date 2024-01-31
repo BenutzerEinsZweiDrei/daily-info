c4967c6b32220cccc5b90177bcc69bbb56deb711
BenutzerEinsZweiDrei <57397036+BenutzerEinsZweiDrei@users.noreply.github.com>
2024-01-31 02:27:24 +0100

    Add files via upload

---
+++ src/ardaudio.py
@@ -0,0 +1,37 @@
+# curl ard audiothek
+
+import requests
+import json
+from datetime import date
+
+def get_ids(thing):
+	url= "https://api.ardaudiothek.de/graphql"
+	headers = {"accept": "application/json", "Content-Type": "application/json", "Origin": "https://api.ardaudiothek.de", "DNT" : "1"}
+	payload = """
+	query { search(query: """+'"'+thing+'"'+""") {items  {nodes  {title, url, publishDate}}}}"""
+	result = requests.post(url, headers=headers, json={"query":  payload})
+	print(result.status_code)
+	return result.json()
+
+def get_item(endpoint):
+	url = endpoint
+	headers = {"accept": "application/json", "Content-Type": "application/json", "Origin": "https://api.ardaudiothek.de", "DNT" : "1"}
+	payload = {}
+	result = requests.get(url, headers=headers, json=payload)
+	print(result.status_code)
+	return result.json()["data"]["item"]
+	
+	
+def search(qu):
+	for item in get_ids(qu)["data"]["search"]["items"]["nodes"]:
+		if "2024-01-30" == item["publishDate"].split(":")[0].split("T")[0]:
+			se_result = get_item(item["url"])
+			# title, sharingUrl
+			return {"title": se_result["title"], "url": se_result["sharingUrl"]}
+		# print(date.today())
+
+def table():
+	tab = []
+	tab.append(search("Ende der Welt Glosse"))
+	tab.append(search("Quarks Daily Wissenspodcast"))
+	return tab
--- src/createmd.py
+++ src/createmd.py
@@ -1,10 +1,12 @@
 import tagesschau
 import blink
+import ardaudio
 from mdutils.mdutils import MdUtils
 from datetime import date
 
 ts = tagesschau.headlines()
 bl = blink.free()
+ard = ardaudio.table()
 
 mdFile = MdUtils(file_name="Readme", title= "Daily Information")
 
@@ -18,6 +20,11 @@ for item in ts:
 mdFile.new_header(2, "Blink")
 
 mdFile.new_line(mdFile.new_inline_link(bl["url"], bl["title"]+": "+ bl["subtitle"]))
+
+mdFile.new_header(2,"täglicher Podcast")
+
+for item in ard:
+	mdFile.new_line(mdFile.new_inline_link(item["url"], item["title"]))
 
 mdFile.create_md_file()
 
