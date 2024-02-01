5f5b5b31f9e12ed1a8e33f4d826f91f9f6eaccc9
BenutzerEinsZweiDrei <57397036+BenutzerEinsZweiDrei@users.noreply.github.com>
2024-02-01 23:36:16 +0100

    Add files via upload

---
+++ src/blinkist/blinkist.py
@@ -0,0 +1,86 @@
+from typing import Dict, List, Optional
+
+from .book import Book
+from .common import api_request_app, api_request_web, scraper
+from .config import HEADERS_ALGOLIA
+from .console import track
+from .curated_list import CuratedList
+
+
+def get_free_daily(locale) -> Book:
+    """
+    Returns the "free daily" book for the given locale.
+
+    NOTE: There might be a subtle difference between `locale` and `language`.
+    I'll stick with Blinkist's naming convention here.
+    """
+    # see also: https://www.blinkist.com/en/content/daily
+    free_daily = api_request_web('free_daily', params={'locale': locale})
+    return free_daily
+
+
+def get_latest_books(limit: Optional[int] = None) -> List[Book]:
+    params = {
+        # 'locale': locale,
+    }
+    if limit:
+        params['limit'] = limit
+    trending_content_items = api_request_web('latest_content_items', params=params)['content_items']
+    return [
+        Book.from_slug(item['slug'])
+        for item in track(trending_content_items, description="Retrieving latest books…")
+        if item['kind'] == 'book'
+    ]
+
+
+def get_trending_books(limit: Optional[int] = None) -> List[Book]:
+    params = {
+        # 'locale': locale,
+    }
+    if limit:
+        params['limit'] = limit
+    trending_content_items = api_request_web('trending_content_items', params=params)['content_items']
+    return [
+        Book.from_slug(item['slug'])
+        for item in track(trending_content_items, description="Retrieving trending books…")
+        if item['kind'] == 'book'
+    ]
+
+
+def get_latest_collections(limit: Optional[int] = None) -> List[CuratedList]:
+    """
+    Returns CuratedLists from the "Latest collections" feed section.
+    NOTE: As of writing, always returns English results.
+    """
+    params = {
+        # 'locale': locale,
+    }
+    if limit:
+        params['limit'] = limit
+    collections = api_request_web('latest_collections', params=params)['collections']  # TODO: add back locale querystring?
+    return [
+        CuratedList.from_slug_or_uuid(collection['slug'])
+        for collection in track(collections, description="Retrieving latest collections…")
+    ]
+
+
+def search_books(query: str, limit: Optional[int] = None, language: Optional[str] = None) -> List[Book]:
+    """
+    Search for books using Blinkist's API.
+    40 results are returned by default.
+    """
+    params: dict[str, str | int] = {
+        'q': query,
+    }
+    if limit:
+        params['limit'] = limit
+    if language:
+        params['lang'] = language
+        params['locale'] = language
+
+    results = api_request_web('search', params=params)['results']
+    return [
+        Book.from_slug(result['slug'])
+        for result in track(results, description="Retrieving search results…")
+        if result['kind'] == 'book'
+    ]
---
+++ src/blinkist/common.py
@@ -0,0 +1,83 @@
+import logging
+from pathlib import Path
+
+import cloudscraper
+import tenacity
+
+from .config import CLOUDFLARE_MAX_ATTEMPTS, CLOUDFLARE_WAIT_TIME, HEADERS
+from .console import track
+
+scraper = cloudscraper.create_scraper()
+
+
+@tenacity.retry(
+    retry=tenacity.retry_if_exception_type(cloudscraper.exceptions.CloudflareChallengeError),
+    wait=tenacity.wait_fixed(CLOUDFLARE_WAIT_TIME),
+    stop=tenacity.stop_after_attempt(CLOUDFLARE_MAX_ATTEMPTS),
+    before_sleep=lambda retry_state: logging.info(f"Retrying in {retry_state.next_action.sleep} seconds…"),
+)
+def request(url, **kwargs):
+    """
+    Wrapper for verifying and retrying GET requests.
+    """
+    kwargs.setdefault('headers', HEADERS)
+    response = scraper.get(url, **kwargs)
+
+    # handle Cloudflare errors
+    # We don't check the reponse content here; it could be large binary data and slow things down.
+    if response.status_code == 403:
+        # TODO: reset scraper for the next try?
+        raise cloudscraper.exceptions.CloudflareChallengeError()
+
+    response.raise_for_status()  # handle other errors
+    return response
+
+
+def api_request(base_url: str, endpoint: str, params=None):
+    """
+    Wrapper for verifying and retrying GET requests to the Blinkist API.
+    Returns the parsed JSON response.
+    Calls `request` internally.
+    """
+    url = base_url + endpoint
+    response = request(url, params=params, headers=HEADERS)
+    return response.json()
+
+
+def api_request_web(endpoint: str, params=None):
+    """
+    Wrapper for verifying and retrying GET requests to the Blinkist web API (https://blinkist.com/api/).
+    Returns the parsed JSON response.
+    """
+    return api_request('https://blinkist.com/api/', endpoint, params=params)
+
+
+def api_request_app(endpoint: str, params=None):
+    """
+    Wrapper for verifying and retrying GET requests to the Blinkist app API (https://api.blinkist.com/).
+    Returns the parsed JSON response.
+    """
+    return api_request('https://api.blinkist.com/', endpoint, params=params)
+
+
+def download(url: str, file_path: Path):
+    """
+    Downloads a file from the given URL to the given path
+    and shows a progress bar!
+    """
+    # adapted from https://stackoverflow.com/a/15645088
+    with open(file_path, 'wb') as f:
+        response = request(url, stream=True)
+        assert response.status_code == 200
+        total_length = response.headers.get('content-length')
+
+        if total_length is None:
+            f.write(response.content)
+        else:
+            for data in track(
+                # FIXME: The displayed numbers won't be easily interpretable.
+                response.iter_content(chunk_size=4096),
+                total=int(total_length) // 4096,
+                description="Downloading…",
+            ):
+                f.write(data)
