280b4137887b67373b194b370cd9b0ef4a2d665a
BenutzerEinsZweiDrei <57397036+BenutzerEinsZweiDrei@users.noreply.github.com>
2024-02-01 23:34:55 +0100

    Add files via upload

--- src/blink.py
+++ src/blink.py
@@ -1,14 +1,17 @@
- # use https://github.com/NicoWeio/blinkist
+# use https://github.com/NicoWeio/blinkist
 
 # import blinkist as library
-import sys
-sys.path.insert(1,"<path to blinkist python>")
-import main
+from blinkist.blinkist import (get_free_daily, get_latest_books,
+                               get_latest_collections, get_trending_books,
+                               search_books)
+
+def get_f():
+	return get_free_daily("de")
 
 def f_sort(free):
 			# title subtitle url
 			return { "title" : free["title"], "subtitle" : free["subtitle"], "url" : free["url"] }
 			
 def free():
-	result = main.get_f()
+	result = get_f()
 	return f_sort(result["book"])
