7562c5469a582fb0f2343121adfe9cfee69595aa
BenutzerEinsZweiDrei <57397036+BenutzerEinsZweiDrei@users.noreply.github.com>
2024-02-01 22:30:03 +0100

    Add files via upload

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -6,17 +6,34 @@ import mediathek
 import parsereddit
 
 def createdata():
-	tab = []
-	tab.append(tagesschau.headlines())
-	tab.append(ardaudio.table())
-	tab.append(mediathek.get())
-	tab.append(parsereddit.table())
-	return tab
+	ts = tagesschau.headlines()
+	ard = ardaudio.table()
+	mt = mediathek.get()
+	red = parsereddit.table()
+	return [ts,ard,mt,red]
+
+def createmd(data):
+	text = "Tagesschau<br>"
+	for item in data.ts:
+		text = text + "["+item["hl"]+"]("+item["url"]+")<br>"
+	text = text + "Podcast<br>"
+	for item in data.ard:
+		text = text + "["+item["title"]+"]("+item["url"]+")<br>"
+	text = text + "Talkshow<br>"
+	text = text + "["+data.mt["title"]+"]("+data.mt["url"]+")<br>"
+	text = text + "Reddit<br>"
+	for item in data.red:
+		text = text + "["+item["title"]+"]("+item["url"]+")<br>"
+	return text
+	
+	
+	
 	
 st.title("Daily Information")
 
 if st.button("Get Data"):
 	load_state = st.text("Loading...")
 	result = createdata()
+	md = createmd(result)
 	load_state.text("Loading...Done!")
-	st.write(result)
+	st.markdown(md)
