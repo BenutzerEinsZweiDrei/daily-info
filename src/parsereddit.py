dd9857a3794eafd2d58eaf89eb78cd39240f172e
BenutzerEinsZweiDrei <57397036+BenutzerEinsZweiDrei@users.noreply.github.com>
2024-02-02 10:53:17 +0100

    Add files via upload

--- src/parsereddit.py
+++ src/parsereddit.py
@@ -1,6 +1,6 @@
 import requests
 import xml.etree.ElementTree as ET
-from datetime import date
+from datetime import date, timedelta
 
 def get_rss(endpoint):
 	url= endpoint
@@ -12,16 +12,19 @@ def get_rss(endpoint):
 	return ET.fromstring(result.text)
 
 def get_data(endpoint):
+	
+	yesterday = (date.today() - timedelta(days=1)).strftime('%m%d%y')
+	
 	for child in get_rss(endpoint):
 		if "entry" in child.tag:
 			if child[6] and child[7] and child[8]:
 				if "published" in child[6].tag:
 					
-					if str(date.today()) == child[6].text.split(":")[0].split("T")[0]:
+					if str(date.today()) == child[6].text.split(":")[0].split("T")[0] or yesterday == child[6].text.split(":")[0].split("T")[0]:
 						return {"title": child[7].text, "url":child[4].attrib["href"]}
 						
 				elif "published" in child[7].tag:
-						if str(date.today()) == child[7].text.split(":")[0].split("T")[0]:
+						if str(date.today()) == child[7].text.split(":")[0].split("T")[0] or yesterday == child[6].text.split(":")[0].split("T")[0]:
 							return {"title": child[8].text, "url":child[5].attrib["href"]}
 					
 def check(endpoint):
